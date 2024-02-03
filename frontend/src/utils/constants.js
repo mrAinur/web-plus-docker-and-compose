1e883dbb868167a5ee3fbd99d530395164e31acd
mrAinur <mr.ainur.02@mail.ru>
2024-02-03 17:27:43 +0500

    Изменил базовый URI на доменное имя

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "https://api.ainur.social.nomoredomainsmonster.ru";
+export const URL = "https://api.ainur.kpd.nomoredomainsmonster.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
